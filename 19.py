cc3591608579f4085467b793eae66aaa0c050d95
Geraint White <geraintwhite@gmail.com>
2015-12-22 13:51:14 +0000

    day 19 parta

---
+++ 19.py
@@ -0,0 +1,5 @@
+import re
+i='CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF'
+f=[l.split(' => ')for l in open('input/19.txt').read().split('\n')[:-1]]
+a=lambda:set(i[:x.start()]+z+i[x.end():]for y,z in f for x in re.finditer(y,i))
+print('parta',len(a()))
