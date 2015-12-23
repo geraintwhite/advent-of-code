aa39749297809e5299a8a662258e7e24d3d30a04
Geraint White <geraintwhite@gmail.com>
2015-12-23 09:18:21 +0000

    Golfed day 19

--- 19.py
+++ 19.py
@@ -1,14 +1,8 @@
-import re
+import re,itertools,functools
+r=functools.reduce
 m='CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF'
 f=[l.split(' => ')for l in open('input/19.txt').read().split('\n')[:-1]]
-a=lambda:set(m[:x.start()]+z+m[x.end():]for y,z in f for x in re.finditer(y,m))
-def b(t):
-    i=0
-    while not t=='e':
-        for y,z in f:
-            if z in t:
-                t=t.replace(z,y,1)
-                i+=1
-    return i
-print('parta',len(a()))
-print('partb',b(m))
+a=lambda t:set(t[:x.start()]+z+t[x.end():]for y,z in f for x in re.finditer(y,t))
+b=lambda t:r(lambda p,_:p if p[-1]=='e'else p[:-1]+r(lambda l,v:l+[l[-1].replace(v[1],v[0],1)] if v[1] in l[-1] else l,f,[p[-1]]),range(len(re.findall(r'[A-Z][a-z]?',m))),[t])
+print('parta',len(a(m)))
+print('partb',len(b(m))-1)
